675e0e44ccce0edfdef09865a680cfff68243547
thea <thsk0001@stud.kea.dk>
2026-02-22 11:14:43 +0100

    linting

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 const port = 8080;
 
@@ -6,16 +6,6 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/timer.html');
 });
 
-app.use("/public", express.static("public")); // middleware til at serve static filer (lydklip)
-
-
-
-
-
-
-
-
-
-
+app.use('/public', express.static('public')); // middleware til at serve static filer (lydklip)
 
 app.listen(port, () => console.log(`Server is running on port: ${port}`));
